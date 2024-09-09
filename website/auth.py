4decb8e5a519305a6dded1c7b503865d27c5ccd3
vijay-seetharam3 <seetharamvijay3@gmail.com>
2024-09-09 18:48:52 +0530

    added a profile section to the task master app

--- website/auth.py
+++ website/auth.py
@@ -34,7 +34,7 @@ def sign_up():
     if request.method == 'POST':
         email = request.form.get('email')
         first_name = request.form.get('firstName')
-        lastName = request.form.get('lastName')
+        last_name = request.form.get('lastName')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
         
@@ -50,7 +50,7 @@ def sign_up():
         elif(len(password1)<7):
             flash('password must have atleast 7 characters',category='error')
         else:
-            new_user = User(email=email,first_name=first_name,password=generate_password_hash(password1))
+            new_user = User(email=email,first_name=first_name,last_name = last_name,password=generate_password_hash(password1))
             db.session.add(new_user)
             print(new_user)
             db.session.commit()
--- website/views.py
+++ website/views.py
@@ -5,27 +5,16 @@ from .models import User, Todo
 
 
 views = Blueprint('views' , __name__)
-"""
-@views.route('/')
-@login_required
-def home():
-    return render_template("home.html",user=current_user)"""
 
 @views.route('/',methods=['POST','GET'])
 @login_required
 def home():
     if request.method == 'POST':
-        print("HI")
         task_content=request.form['content']
         new_task = Todo(content=task_content,user_id=current_user.id)
-        print(new_task)
-
         try:
-            print("bye")
             db.session.add(new_task)
-            print("hi")
             db.session.commit()
-            print("commited")
             return redirect('/')
         except Exception as e:
             # Print the exception details to understand the issue
@@ -35,6 +24,7 @@ def home():
         tasks = Todo.query.filter_by(user_id=current_user.id).order_by(Todo.date_created).all()
         return render_template('home.html',tasks=tasks,user=current_user)
 @views.route('/delete/<int:id>')
+@login_required
 def delete(id):
     task_to_delete = Todo.query.get_or_404(id)
 
@@ -45,8 +35,11 @@ def delete(id):
     except:
         return 'There was a problem in deleting'
 @views.route('/update/<int:id>',methods=['GET','POST'])
+@login_required
 def update(id):
+    print(id)
     task_to_update = Todo.query.get_or_404(id)
+    print(task_to_update)
     if request.method == 'POST':
         task_to_update.content=request.form['content']
 
@@ -57,3 +50,28 @@ def update(id):
             return 'There is an error updating'
     else:
         return render_template('update.html',task_to_update=task_to_update,user=current_user)
+@views.route('/profile',methods=['GET'])
+@login_required
+def profile():
+    return render_template('profile.html',user=current_user)
+
+
+@views.route('/profile/update/<int:id>',methods=['GET','POST'])
+@login_required
+def pupdate(id):
+    user = User.query.get_or_404(id) 
+
+    if request.method == 'POST':
+        user.first_name = request.form.get('first_name')
+        user.last_name = request.form.get('last_name')
+        user.email = request.form.get('email')
+
+        
+        try:
+            db.session.commit()
+            return redirect('/profile')
+        except Exception as e:
+            print(f"Error updating profile: {e}")
+            return 'There was an error updating your profile'
+    else:
+        return render_template('profile.html', user=current_user)
